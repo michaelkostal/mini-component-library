6267805c2ea43ad8f7b4b01ad4a973da3b977aba
Michael Kostal <kostal@gmail.com>
2023-12-30 16:13:54 -0600

    basic

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,15 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  'small': {
+    'size':12
+  }, 
+  'large': {
+    'size':16
+  }
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +22,36 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const styles = SIZES[size];
+  return <Wrapper style={{"--width":width + "px"}}>
+    <TheIcon id={icon} size={styles.size}/>
+    <Label>
+        <VisuallyHidden>{label}</VisuallyHidden>
+        <Input type="text" placeholder={placeholder} />
+    </Label>
+  </Wrapper>;
 };
+
+const Wrapper = styled.div`
+  position:relative;
+  width: var(--width);
+`
+const TheIcon = styled(Icon)`
+  position:absolute;
+  left:0;
+  top:0;
+`
+
+const Label = styled.label`
+  
+`;
+
+const Input = styled.input`
+  width:100%;
+  margin:0;
+  padding:0 0 12px 24px;
+  border:0;
+  border-bottom:1px solid black;
+`
 
 export default IconInput;

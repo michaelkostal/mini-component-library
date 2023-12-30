e93d7d2b6da36871e5f2c8d58bff275f9105c9fb
Michael Kostal <kostal@gmail.com>
2023-12-30 16:37:01 -0600

    Update IconInput.js

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -8,16 +8,16 @@ import VisuallyHidden from '../VisuallyHidden';
 
 const SIZES = {
   'small': {
-    'size':12,
-    'fontSize':14,
-    'padding':'0 0 0 16px',
-    'borderBottom':'1px'
+    size:12,
+    fontSize:14,
+    padding:'0 0 0 16px',
+    borderBottom:'1px'
   }, 
   'large': {
-    'size':16,
-    'fontSize':18,
-    'padding':'0 0 0 20px',
-    'borderBottom':'2px'
+    size:16,
+    fontSize:18,
+    padding:'0 0 0 24px',
+    borderBottom:'2px'
   }
 }
 
@@ -26,29 +26,23 @@ const IconInput = ({
   icon,
   width = 250,
   size,
-  placeholder,
+  ...delegated
 }) => {
   const styles = SIZES[size];
-  return <Wrapper style={{"--width":width + "px"}}>
+  return <Label style={{"--width":width + "px"}}>
     <TheIcon id={icon} size={styles.size}/>
-    <Label>
         <VisuallyHidden>{label}</VisuallyHidden>
-        <Input style={
-          {
+        <TextInput  
+          {...delegated} 
+          style={{
             "--fontSize":`${(styles.fontSize/16)}rem`,
             "--padding":styles.padding,
             "--borderBottom":styles.borderBottom
-          }
-        } 
-        type="text" placeholder={placeholder} />
+          }} 
+        />
     </Label>
-  </Wrapper>;
 };
 
-const Wrapper = styled.div`
-  position:relative;
-  width: var(--width);
-`
 const TheIcon = styled(Icon)`
   position:absolute;
   left:0;
@@ -56,16 +50,21 @@ const TheIcon = styled(Icon)`
 `
 
 const Label = styled.label`
-  
+    position:relative;
+    width: var(--width);
 `;
 
-const Input = styled.input`
+const TextInput = styled.input`
   width:100%;
   margin:0;
   padding:var(--padding);
   border:0;
   border-bottom:var(--borderBottom) solid black;
   font-size:var(--fontSize);
+  font-weight:700;
+  &::placeholder{
+    font-weight:400;
+  }
 `
 
 export default IconInput;
